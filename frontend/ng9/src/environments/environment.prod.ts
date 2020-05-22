46c547984de9e31bc6bc0d9685684e48f0698c54
simon <simon.kaier@gmail.com>
2020-05-22 14:41:46 +0200

    moved google config key to excluded source

--- frontend/ng9/src/environments/environment.prod.ts
+++ frontend/ng9/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+import { googlApiKey } from './config.local';
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: googlApiKey,
+    authDomain: 'snippeter-eddf3.firebaseapp.com',
+    databaseURL: 'https://snippeter-eddf3.firebaseio.com',
+    projectId: 'snippeter',
+    storageBucket: 'snippeter.appspot.com',
+    messagingSenderId: '710864936160',
+    appId: '1:710864936160:web:801b66376bc6e728'
+  }
 };
